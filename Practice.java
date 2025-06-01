1f86770c6079dc7d3c151980c732a3b7fdfa2263
dhruv-vaniya19 <dhruvrajvaniya03@gmail.com>
2025-06-01 20:02:20 +0530

    Sum of Two Variables

--- Practice.java
+++ Practice.java
@@ -1,5 +1,10 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
     }
 }
